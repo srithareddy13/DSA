5bd2f3dbc6b367a92c82646240cd510aa76aca9d
srithareddy13 <maramreddysrithareddy_23it@student.vardhaman.org>
2026-07-06 14:40:53 +0530

    Implement GCD function for an array of numbers

---
+++ GCDofArray.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findGCD(self, nums):
+        small = min(nums)
+        large = max(nums)
+        for i in range(small, 0, -1):
+            if small % i == 0 and large % i == 0:
+                return i
