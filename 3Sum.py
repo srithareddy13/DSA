89fcf833de023485d77bb9dd5f9edb519f92e83a
srithareddy13 <maramreddysrithareddy_23it@student.vardhaman.org>
2026-08-04 20:01:58 +0530

    Add threeSum function to find zero-sum triplets
    Implement threeSum method to find triplets that sum to zero.

---
+++ 3Sum.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        ans = []
+        n = len(nums)
+
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+
+                if s == 0:
+                    ans.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+                elif s < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return ans
