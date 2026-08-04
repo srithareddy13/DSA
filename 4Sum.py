b57f5ed0dcc3c9f024c845674487413bcafd059c
srithareddy13 <maramreddysrithareddy_23it@student.vardhaman.org>
2026-08-04 20:04:28 +0530

    Implement fourSum function to find quadruplets

---
+++ 4Sum.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        nums.sort()
+        ans = []
+        n = len(nums)
+
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = n - 1
+
+                while left < right:
+                    s = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if s == target:
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+
+                        left += 1
+                        right -= 1
+
+                    elif s < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return ans
