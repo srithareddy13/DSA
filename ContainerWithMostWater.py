7268f6b3e56a4e76dcbf3a41146ca93c7b0b9a34
srithareddy13 <maramreddysrithareddy_23it@student.vardhaman.org>
2026-08-04 20:02:34 +0530

    Implement maxArea function to calculate water container

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        ans = 0
+
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
+            ans = max(ans, area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return ans
